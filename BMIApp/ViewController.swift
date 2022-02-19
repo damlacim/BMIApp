5ac3049b950cae3a8c29a0cbebc439f089a08172
damlacim <cim.damla@gmail.com>
2022-02-19 16:24:05 +0300

    Initial Commit

---
+++ BMIApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMIApp
+//
+//  Created by Damla Çim on 19.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
